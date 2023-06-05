24da52fb979a1e3c256de82cba7a5bbaa08ce0fc
alonyer1 <95702072+alonyer1@users.noreply.github.com>
2023-06-05 16:46:10 +0300

    yes

---
+++ oracle-mode/plot.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def histogram(bucket_size, input_files):
+    data = []  # This will hold the data read from each file
+    data2 = []
+    min_value = float('inf')
+    max_value = float('-inf')
+    
+    for i, file in enumerate(input_files):
+        with open(file, 'r') as f:
+            print("file",i,"loaded.\n")
+            numbers = list(map(float, f.readlines()[1:]))  # Read numbers, starting from second line
+            numbers2 = [t/1000.0 for t in numbers]
+            data.append(numbers)
+            data2.append(numbers2)
+            min_value = min(min_value, min(numbers))  # Track min and max across all files
+            max_value = max(max_value, max(numbers))
+
+    # Compute the bucket edges (these are shared across all histograms)
+    bins = np.arange(min_value, 200, bucket_size)
+    #bins = np.append(bins, max_value)  # Make sure the max value falls within the last bin
+    fig, ax1 = plt.subplots()
+    print("Buckets arranged.\n")
+    # Plot the histograms
+    colors = ['red', 'yellow', 'blue', 'purple']
+    ax1.hist(data,bins, log=True, label=[f'String difference at location {i*5}' for i in range(len(data))])
+    '''for i, numbers in data.items():
+        ax1.hist(numbers, bins, alpha=.7, color=colors[i], edgecolor='black', label=f'String difference at location {i*5}')
+        print("Buckets arranged.\n")'''
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("% of all calls to 'func'",fontsize=18)
+    #ax2.tick_params(axis='y')
+    #ax2.hist(data2,bins, alpha=1.0, label=[f'String difference at location {i}' for i in range(len(data))])
+    ax1.set_ylim(0, 100000)
+    ax2.set_ylim(0, 100)
+    print("Buckets arranged2.\n")
+    #plt.yscale('log')
+    ax1.set_xlabel("\nDelta of time before and after function execution (#cycles)", fontsize=18)
+    ax1.set_ylabel("Number of calls to 'func'",fontsize=18)
+    ax1.legend(fontsize=14)
+    plt.title("Runtime of a function that compares 2 strings char-by-char\n(Intentional cache misses)", fontsize=20)
+    plt.show()
+
+if __name__ == "__main__":
+    input_files=["results/cache-00.txt"]
+    for i in range(4,12,5):
+        input_files.append("results/cache-"+str(i)+".txt")
+    histogram(1, input_files)
